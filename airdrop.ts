7a7361b55fec6ecf3a48af3588331ce88aacba19
ayberklevendoglu <ayberklevendoglu@gmail.com>
2024-07-15 22:42:13 +0300

    refactor code / finish empty wallet

--- airdrop.ts
+++ airdrop.ts
@@ -7,16 +7,18 @@ const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 const connection = new Connection("https://api.devnet.solana.com");
 
 (async () => {
-    // Check balance
-    const balance = await connection.getBalance(keypair.publicKey);
-    console.log("Your balance is: ", balance / LAMPORTS_PER_SOL);
-    try {
-        
-        // We're going to claim 2 devnet SOL tokens
-        const txhash = await connection.requestAirdrop(keypair.publicKey, 2 * LAMPORTS_PER_SOL);
-        console.log(`Success! Check out your TX here: 
+  // Check balance
+  const balance = await connection.getBalance(keypair.publicKey);
+  console.log("Your balance is: ", balance / LAMPORTS_PER_SOL);
+  try {
+    // We're going to claim 2 devnet SOL tokens
+    const txhash = await connection.requestAirdrop(
+      keypair.publicKey,
+      2 * LAMPORTS_PER_SOL
+    );
+    console.log(`Success! Check out your TX here: 
         https://explorer.solana.com/tx/${txhash}?cluster=devnet`);
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
---
+++ emptyWallet.ts
@@ -0,0 +1,51 @@
+import {
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  SystemProgram,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import wallet from "/home/bolti/.config/solana/risein.json";
+
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const connection = new Connection("https://api.devnet.solana.com");
+const toKeypair = Keypair.generate();
+
+(async () => {
+  const balance = await connection.getBalance(keypair.publicKey);
+  console.log("Your balance is: ", balance / LAMPORTS_PER_SOL);
+
+  const transaction = new Transaction().add(
+    SystemProgram.transfer({
+      fromPubkey: keypair.publicKey,
+      toPubkey: toKeypair.publicKey,
+      lamports: balance,
+    })
+  );
+
+  transaction.recentBlockhash = (
+    await connection.getLatestBlockhash()
+  ).blockhash;
+  transaction.feePayer = keypair.publicKey;
+
+  const fee = (await connection.getFeeForMessage(transaction.compileMessage()))
+    .value;
+
+  transaction.instructions.pop();
+
+  if (fee != null) {
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: keypair.publicKey,
+        toPubkey: toKeypair.publicKey,
+        lamports: balance - fee,
+      })
+    );
+  }
+
+  const txSignature = await sendAndConfirmTransaction(connection, transaction, [
+    keypair,
+  ]);
+  console.log("Transaction signature: ", txSignature);
+})();
